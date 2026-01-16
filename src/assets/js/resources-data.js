fb5439e6f6bb9ee8e3b129bd56858cab4e9231c4
JM Chris Chang <jmchrischang@gmail.com>
2026-01-16 15:45:50 -0500

    database merge, updated search faceting

--- src/assets/js/resources-data.js
+++ src/assets/js/resources-data.js
@@ -2,17 +2,25 @@
 layout: none
 ---
 // Generated resources data for faceted search
-const resourcesData = [
-  {% for resource in site.resources %}
+// Total resources: {{ site.resources.size }}
+const resourcesData = [{% for resource in site.resources %}
   {
-    objectid: "{{ resource.objectid }}",
+    objectid: {{ resource.objectid | jsonify }},
     title: {{ resource.title | jsonify }},
     alternatetitle: {{ resource.alternatetitle | jsonify }},
     url: "{{ resource.url }}",
-    external_url: {{ resource.external_url | jsonify }},
+    externalurl: {{ resource.externalurl | jsonify }},
     category: {{ resource.category | jsonify }},
     institution: {{ resource.institution | jsonify }},
-    description: {{ resource.description | jsonify }}
-  }{% unless forloop.last %},{% endunless %}
-  {% endfor %}
+    access: {{ resource.access | jsonify }},
+    sourcelist: {{ resource.sourcelist | jsonify }},
+    tags: {{ resource.tags | jsonify }},
+    description: {{ resource.description | jsonify }},
+    lastmodified: {{ resource.lastmodified | jsonify }}
+  }{% unless forloop.last %},{% endunless %}{% endfor %}
 ];
+
+// Verify data loaded
+if (typeof resourcesData !== 'undefined') {
+  console.log('Resources data loaded:', resourcesData.length, 'items');
+}
